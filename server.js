3f690352ce51d92ecb369260954cea3c2653d9be
samelie <sam@stinkdigital.com>
2016-01-06 23:58:43 +0000

    dy y

--- server.js
+++ server.js
@@ -31,6 +31,10 @@ var EXPRESS = (function() {
     request('http://images1.wikia.nocookie.net/__cb20120715102950/disney/images/a/a5/Disneygoofy2012.jpeg').pipe(res);
   });
 
+  app.get('/goofy2', function(req, res) {
+    request('https://s3-eu-west-1.amazonaws.com/chromegno.me/images/locations/08.jpg').pipe(res);
+  });
+
   app.get('/getImage', function(req, res) {
     request("https://s3-eu-west-1.amazonaws.com/chromegno.me/bergsjostolen.jpg").pipe(res);
   });
@@ -83,6 +87,43 @@ var EXPRESS = (function() {
       });
     });
   })
+
+
+  app.get('/getVideo2', function(req, res) {
+    DL.getSidx().then(function(data) {
+      var url = data.url + '&range=' + data.range;
+      console.log(data.range, data.max);
+      res.writeHead(206, {
+        'Content-Range': 'bytes ' + data.range + '/' + data.max,
+        //'Content-Range': 'bytes ' + range + '/27908',
+        //'Content-Length': '27908',
+        'Content-Length': data.max,
+        'Accept-Ranges': 'bytes',
+        'Content-Type': 'video/mp4',
+        "Access-Control-Allow-Origin": "*"
+      });
+      console.log(url);
+      var r = request({
+        url: url,
+        //url: 'https://radvisions.s3-eu-west-1.amazonaws.com/2b173550-a6b9-11e5-a7b6-b9c2f8eca471'
+      }).on('response', function(response) {
+
+        response.on('data', function(data) {
+          res.write(data);
+          console.log("data chunk received: " + data.length)
+        });
+
+        response.on('end', function(data) {
+          res.end();
+          console.log('Video completed');
+        });
+      });
+
+      r.on('error', function(err) {
+        console.log(err);
+      });
+    });
+  });
 
 
   //routes
