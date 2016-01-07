f0bc899d3093238e73cea42126dc39abe49150ea
samelie <sam@stinkdigital.com>
2016-01-07 01:03:52 +0000

    sd

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 window.onload = function() {
   setTimeout(function() {
     var player = new Player(document.getElementById('myVid'));
+    document.getElementById('myVid2').src = "http://52.90.55.176/staticVideo";
   }, 1000);
 }
